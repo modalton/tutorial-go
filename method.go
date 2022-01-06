2568a5d98ad7ca473bd30ad6004948decad443fe
Michael Dalton <modintexas@gmail.com>
2022-01-06 13:54:38 -0800

    New stuff

--- method.go
+++ method.go
@@ -1,30 +1,15 @@
 package tutorial
 
-import (
-	"fmt"
-	"math"
-)
-
 // BrokenMethod has a bug - it will try to read the 4th
 // index of Data even when it only has a length of 3.
 func BrokenMethod(Data string) bool {
-	return len(Data) >= 3 &&
-		Data[0] == 'F' &&
-		Data[1] == 'U' &&
-		Data[2] == 'Z' &&
-		Data[3] == 'Z'
+	return len(Data) >= 99
 }
 
 type Test struct {
 }
 
 func BadLen(Data int) string {
-	arr := make([]Test, Data)
-	lenboi := len(arr)
-	fmt.Printf("len %d \n", lenboi)
-	arr = append(arr, Test{})
-	lenboi = len(arr)
-	fmt.Printf("len %d shouldoverflow %+v \n", lenboi, lenboi > math.MaxInt64)
 
 	return "yes"
 
