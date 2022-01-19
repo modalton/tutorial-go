2ad34a0742d6b28b243f1700cb2b3bd3d578d106
Michael Dalton <modintexas@gmail.com>
2022-01-18 16:05:44 -0800

    No arr err

--- method.go
+++ method.go
@@ -8,11 +8,7 @@ import (
 // BrokenMethod has a bug - it will try to read the 4th
 // index of Data even when it only has a length of 3.
 func BrokenMethod(Data string) bool {
-	return len(Data) >= 3 &&
-		Data[0] == 'B' &&
-		Data[1] == 'U' &&
-		Data[2] == 'Z' &&
-		Data[3] == 'Z'
+	return len(Data) >= 3
 }
 
 type Test struct {
