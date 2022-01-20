efcd020697f0a271f1e1ef652362b1ad68157593
Michael Dalton <modintexas@gmail.com>
2022-01-19 19:02:00 -0800

    Duzz

--- method.go
+++ method.go
@@ -9,7 +9,7 @@ import (
 // index of Data even when it only has a length of 3.
 func BrokenMethod(Data string) bool {
 	return len(Data) >= 3 &&
-		Data[0] == 'F' &&
+		Data[0] == 'D' &&
 		Data[1] == 'U' &&
 		Data[2] == 'Z' &&
 		Data[3] == 'Z'
