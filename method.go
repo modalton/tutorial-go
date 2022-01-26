5b5db62afc84d5abc097827c7afca51c2a84a974
Michael Dalton <modintexas@gmail.com>
2022-01-26 12:03:32 -0800

    Bugs are back

--- method.go
+++ method.go
@@ -3,5 +3,9 @@ package tutorial
 // BrokenMethod has a bug - it will try to read the 4th
 // index of Data even when it only has a length of 3.
 func BrokenMethod(Data string) bool {
-	return len(Data) >= 3
+	return len(Data) >= 3 &&
+		Data[0] == 'B' &&
+		Data[1] == 'A' &&
+		Data[2] == 'C' &&
+		Data[3] == 'K'
 }
