08a907bda4ee90801749d40ab34037b003d86b3f
Michael Dalton <modintexas@gmail.com>
2022-05-10 11:54:09 -0700

    re Texd

--- method.go
+++ method.go
@@ -3,7 +3,7 @@ package tutorial
 // BrokenMethod has a bug - it will try to read the 4th
 // index of Data even when it only has a length of 3.
 func BrokenMethod(Data string) bool {
-	if Data == "tex" {
+	if Data == "smalltex" {
 		panic("uh oh")
 	}
 	return len(Data) >= 3 &&
