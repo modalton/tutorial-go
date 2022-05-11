67d184b020dc470da8d96688df9840376d9ee866
Everest Stefan Munro-Zeisberger <evmunro1@gmail.com>
2022-05-11 11:25:03 -0700

    Update method.go

--- method.go
+++ method.go
@@ -2,6 +2,7 @@ package tutorial
 
 // BrokenMethod has a bug - it will try to read the 4th
 // index of Data even when it only has a length of 3.
+// test
 func BrokenMethod(Data string) bool {
 	if Data == "bigtex!" {
 		panic("uh oh")
