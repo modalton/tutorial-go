1700e32b86cdeddc63de8e8d4d31ecb4298fda81
Michael Dalton <modintexas@gmail.com>
2022-05-18 10:21:34 -0700

    check

--- method.go
+++ method.go
@@ -3,14 +3,13 @@ package tutorial
 // BrokenMethod has a bug - it will try to read the 4th
 // index of Data even when it only has a length of 3.
 func BrokenMethod(Data string) bool {
-	if Data == "bigtex!" {
+	if Data == "tex" {
 		panic("uh oh")
 	}
-	return len(Data) >= 3 &&
+	return len(Data) >= 2 &&
 		Data[0] == 'F' &&
 		Data[1] == 'i' &&
-		Data[2] == 'x' &&
-		Data[3] == 'd'
+		Data[2] == 'n'
 }
 
 type Test struct {
