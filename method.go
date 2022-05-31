fcb06967fe32ad83852e402a5bac1d6670d6dbbb
Michael Dalton <modintexas@gmail.com>
2022-05-31 08:29:53 -0700

    needs unstickin

--- method.go
+++ method.go
@@ -3,14 +3,15 @@ package tutorial
 // BrokenMethod has a bug - it will try to read the 4th
 // index of Data even when it only has a length of 3.
 func BrokenMethod(Data string) bool {
-	if Data == "bigtex!" {
-		panic("uh oh")
+	if Data == "F" {
+		panic("new bug")
 	}
-	return len(Data) >= 3 &&
+	return len(Data) >= 4 &&
 		Data[0] == 'F' &&
 		Data[1] == 'i' &&
 		Data[2] == 'x' &&
-		Data[3] == 'd'
+		Data[3] == 'd' &&
+		Data[4] == 'n'
 }
 
 type Test struct {
